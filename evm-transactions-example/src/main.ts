9508c39648d876ca3a48fa497a31810082e4abf8
mdmuzaffar914 <82084758+mdmuzaffar914@users.noreply.github.com>
2023-10-01 17:36:27 +0600

    Update main.ts

--- evm-transactions-example/src/main.ts
+++ evm-transactions-example/src/main.ts
@@ -1,25 +1,46 @@
 import {TypeormDatabase} from '@subsquid/typeorm-store'
+import { Gravatar } from './model'
 import {processor} from './processor'
-import {Transaction} from './model'
-
+import * as GravatarABI from "./abi/Gravity";
 processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
-    let transactions: Transaction[] = []
-
-    for (let block of ctx.blocks) {
-        for (let transaction of block.transactions) {
-            transactions.push(
-                new Transaction({
-                    id: transaction.id,
-                    block: block.header.height,
-                    timestamp: new Date(block.header.timestamp),
-                    from: transaction.from || '0x',
-                    to: transaction.to || '0x',
-                    hash: transaction.hash,
-                    input: transaction.input,
-                })
-            )
+    const gravatars: Map<string, Gravatar> = new Map();
+    for (let c of ctx.blocks) {
+        for (let log of c.logs) {
+            let { idString, owner, displayName, imageUrl } = extractData(log);
+            let gravatar =new Gravatar({
+                id: idString,
+                owner,
+                displayName,
+                imageUrl,
+            })
+              gravatars.set(idString, gravatar);
+            }
+           
         }
-    }
+    
+    await ctx.store.upsert([...gravatars.values()]);
+});
 
-    await ctx.store.insert(transactions)
-})
+function extractData(log: any): { 
+    idString: string, 
+    owner: string, 
+    displayName: string, 
+    imageUrl: string 
+} {;
+    if (log.topics[0] === GravatarABI.events.NewGravatar.topic) { 
+        let{id, owner, displayName, imageUrl} = GravatarABI.events.NewGravatar.decode(log);
+        let idString = id.toString(16);
+        return{idString, owner, displayName, imageUrl };  
+    
+    }
+    if (log.topics[0] === GravatarABI.events.UpdatedGravatar.topic) { 
+        let{id, owner, displayName, imageUrl} = 
+         GravatarABI.events.UpdatedGravatar.decode(log);
+    let idString = id.toString(16);
+    return{idString, owner, displayName, imageUrl };
+    
+    }
+      throw new Error("Unsupported topic");
+    }
+  
+
