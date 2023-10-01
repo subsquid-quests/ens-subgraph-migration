3b75cb5fdf95cd6015591c223815aed084d41f49
mdmuzaffar914 <82084758+mdmuzaffar914@users.noreply.github.com>
2023-10-01 17:35:31 +0600

    Update processor.ts

--- evm-transactions-example/src/processor.ts
+++ evm-transactions-example/src/processor.ts
@@ -1,27 +1,47 @@
-import {EvmBatchProcessor} from '@subsquid/evm-processor'
 import {lookupArchive} from '@subsquid/archive-registry'
-
-const ACCOUNT_ADDRESS = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'
-
+import {
+    BlockHeader,
+    DataHandlerContext,
+    EvmBatchProcessor,
+    EvmBatchProcessorFields,
+    Log as _Log,
+    Transaction as _Transaction,
+} from '@subsquid/evm-processor'
+import * as Gravatar from "./abi/Gravity";
 export const processor = new EvmBatchProcessor()
     .setDataSource({
+        // Change the Archive endpoints for run the squid
+        // against the other EVM networks
+        // For a full list of supported networks and config options
+        // see https://docs.subsquid.io/evm-indexing/
         archive: lookupArchive('eth-mainnet'),
+
+        // Must be set for RPC ingestion (https://docs.subsquid.io/evm-indexing/evm-processor/)
+        // OR to enable contract state queries (https://docs.subsquid.io/evm-indexing/query-state/)
         chain: 'https://rpc.ankr.com/eth',
     })
-    .setFinalityConfirmation(10)
+    .setFinalityConfirmation(75)
     .setFields({
         transaction: {
             from: true,
             value: true,
             hash: true,
-            input: true,
         },
     })
-    // Txs sent to vitalik.eth
-    .addTransaction({
-        to: [ACCOUNT_ADDRESS],
+    .setBlockRange({
+        from: 6_000_000,
     })
-    // Txs sent from vitalik.eth
-    .addTransaction({
-        from: [ACCOUNT_ADDRESS],
-    })
+
+    .addLog({
+        address: ["0x2E645469f354BB4F5c8a05B3b30A929361cf77eC"],
+        topic0: [
+            Gravatar.events.NewGravatar.topic, 
+            Gravatar.events.UpdatedGravatar.topic,
+        ],
+    });
+
+export type Fields = EvmBatchProcessorFields<typeof processor>
+export type Block = BlockHeader<Fields>
+export type Log = _Log<Fields>
+export type Transaction = _Transaction<Fields>
+export type ProcessorContext<Store> = DataHandlerContext<Store, Fields>
